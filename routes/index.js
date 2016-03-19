db445f2d95c9204a4b83c3edb1a0d674c1cd7987
Oleg Dulin <oleg.dulin@gmail.com>
2016-03-19 07:14:04 -0400

    Added redis

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(JSON.stringify(req));
+router.get('/*', function(req, res, next) {
+  var gatewayRequest={
+    headers: req.headers,
+    path: req.url, //TODO Take the substring
+    method: req.method,
+    query: req.query
+  }
+  console.log(gatewayRequest);
   res.render('index', { title: 'Express' });
 });
 
